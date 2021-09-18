e9f762233da20659d6855898b9d2a7fd9a11439f
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-18 17:56:16 +0300

    Update src/index.js imports and apply Tailwind

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { reducer } from "./utils/reducers";
+import { store } from "./utils/store";
+import * as serviceWorker from "./serviceWorker";
+import Home from "./pages/Home";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
