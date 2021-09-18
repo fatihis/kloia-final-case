bcd92415ddc7a57363b30ef763178f08d352f197
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-18 18:21:33 +0300

    Add actions for reducer

--- src/utils/reducers/index.js
+++ src/utils/reducers/index.js
@@ -4,11 +4,26 @@ const reducerInitialState = { coffees };
 
 export const reducer = (state = reducerInitialState, action) => {
   switch (action.type) {
-    case "getAll":
+    case GETALL:
       return state;
-    case "getSome":
+    case GETALL:
       return state;
     default:
       return state;
   }
+};
+
+/* Since actions is limited did not separated from reducers itself
+   IF any more couple of actions needed, then separate it to another folder for not to be any complex
+*/
+
+export const ACTIONS = [
+  {
+    GETALL: "GETALL",
+    GETSINGLE: "GETSINGLE",
+  },
+];
+
+const getSingle = (id) => {
+  useSearch();
 };
