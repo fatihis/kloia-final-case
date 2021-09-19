a9bc15044a0c7615789e98ddbc5facac84f2a707
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-19 14:58:11 +0300

    Apply fonts

--- src/components/BaseComponents/SimpleTextBlock/index.js
+++ src/components/BaseComponents/SimpleTextBlock/index.js
@@ -5,11 +5,13 @@ export const SimpleTextBlock = (props) => {
   return (
     <div className="flex-1 flex items-center mb-1 ">
       {props.type === "header" ? (
-        <h4 className=" font-extrabold text-2xl mb-1">{props.text}</h4>
+        <h4 className=" exo-bold font-extrabold  text-2xl mb-1">
+          {props.text}
+        </h4>
       ) : props.type === "description" ? (
-        <p className=" text-xl mb-1">{props.text}</p>
+        <p className=" roboto-regular text-xl mb-1">{props.text}</p>
       ) : props.type === "annotation" ? (
-        <p className="">{props.text}</p>
+        <p className="roboto-regular">{props.text}</p>
       ) : null}
     </div>
   );
