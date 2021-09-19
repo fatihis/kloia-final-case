1b09e30089cd0993f3c74228d378f4162fd5f24c
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-19 13:48:09 +0300

    Add Menu Component (parent of MenuItem)

---
+++ src/components/ComponentContainers/Menu/index.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { ListingContext } from "../../../utils/contexts/ListingContext/ListingContext";
+import { useSearch } from "../../../utils/useSearch";
+import { MenuItem } from "../../ComponentBlocks/MenuItem";
+
+export const Menu = (props) => {
+  const listingContext = useContext(ListingContext);
+  const elements = useSearch(props.list, listingContext.searchString).data;
+  return (
+    <div className="w-full px-16 flex flex-col min-h-full pt-8">
+      {elements.map((element) => {
+        return <MenuItem item={element} />;
+      })}
+    </div>
+  );
+};
