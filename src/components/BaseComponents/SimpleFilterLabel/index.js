6a6637420821a56b6c7d9f51e57ab2ec52ee34dc
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-19 13:44:32 +0300

    Update SimpleFilterLabel, refactor code

--- src/components/BaseComponents/SimpleFilterLabel/index.js
+++ src/components/BaseComponents/SimpleFilterLabel/index.js
@@ -1,14 +1,18 @@
 import "./styles/styles.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export const SimpleFilterLabel = (props) => {
+  const [category, setCategory] = useState(props.text);
   return (
-    <div className="w-full flex items-center justify-center h-4">
+    <div
+      onClick={(category) => props.onClick(category)}
+      className="w-full flex items-start justify-start h-4 mb-2"
+    >
       {props.isActive ? (
-        <p className="select-none active font-bold">{props.text}</p>
+        <p className=" active font-bold">{category}</p>
       ) : (
-        <p className="select-none passive">{props.text}</p>
+        <p className=" passive">{category}</p>
       )}
     </div>
   );
