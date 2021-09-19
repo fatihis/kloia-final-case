03160b148e41835557cc60e736788dc28f071814
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-19 13:45:37 +0300

    Update SimpleTextBlock layout

--- src/components/BaseComponents/SimpleTextBlock/index.js
+++ src/components/BaseComponents/SimpleTextBlock/index.js
@@ -3,11 +3,11 @@ import "./styles/styles.css";
 //Props : {Types : "header","description", "annotation" }
 export const SimpleTextBlock = (props) => {
   return (
-    <div className="flex-1 flex items-center mb-4">
+    <div className="flex-1 flex items-center mb-1 ">
       {props.type === "header" ? (
-        <h4 className="font-bold text-2xl">{props.text}</h4>
+        <h4 className=" font-extrabold text-2xl mb-1">{props.text}</h4>
       ) : props.type === "description" ? (
-        <p className="font-semibold text-xl">{props.text}</p>
+        <p className=" text-xl mb-1">{props.text}</p>
       ) : props.type === "annotation" ? (
         <p className="">{props.text}</p>
       ) : null}
